8cc9993f75c87436d3c551d0c2d4e529431f650d
Tyler <tylergross28@gmail.com>
2023-12-30 14:42:16 -0500

    Initial Commit

---
+++ extract-v3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  extract-v3
+//
+//  Created by Tyler Gross on 12/30/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
